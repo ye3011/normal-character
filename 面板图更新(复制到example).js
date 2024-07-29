dc8af13dd0cf9904c083a693c9af901621bf574d
小夜夜 <12703330+grey-night-123@user.noreply.gitee.com>
2024-07-30 04:51:45 +0800

    复制到example，指令#面板图更新

---
+++ 面板图更新(复制到example).js
@@ -0,0 +1,44 @@
+import { exec, execSync } from 'child_process'
+import fs from 'fs'
+const Path = './plugins/miao-plugin/resources/profile/normal-character'
+const url = 'https://github.com/ye3011/normal-character'
+export class Updatenormal extends plugin {
+  constructor () {
+    super({
+      name: '面板图更新',
+      dsc: '更新角色面板图',
+      event: 'message',
+      priority: 100,
+      rule: [
+        {
+          reg: '^#(强制)?面板图(更新|下载)$',
+          fnc: 'Updatenormal'
+        }
+      ]
+    })
+  }
+
+  async Updatenormal (e) {
+    let cmd = ''
+    if (!fs.existsSync(Path) || this.e.msg.includes('下载')) {
+      await this.reply(`开始下载${url}`)
+      cmd = `git clone --depth=1 ${url} ${Path}`
+      exec(cmd, { cwd: process.cwd(), stdio: 'inherit' }, (error) => {
+        if (error) { return this.reply(`下载错误：\n${error}`) } else {
+          this.reply('面板图下载完成')
+        }
+      })
+    } else {
+      await this.reply(`更新中，耐心等待，保存路径${Path}`)
+      cmd = 'git pull'
+      if (this.e.msg.includes('强制')) { execSync('git fetch && git reset --hard', { cwd: Path }) }
+      exec(cmd, { cwd: Path, stdio: 'inherit' }, (output, error) => {
+        if (error) {
+          if (error.match(/Already up to date\./)) { this.reply('当前面板图已是最新') } else {
+            this.reply('面板图更新结束')
+          }
+        } else { return this.reply(`更新错误：${output}`) }
+      })
+    }
+  }
+}
